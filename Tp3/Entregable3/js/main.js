03248ebf99c8199228f013a1287353168a224bdc
canadanicolas <54717064+canadanicolas@users.noreply.github.com>
2021-10-22 17:03:23 -0300

    enemy movement added

--- Tp3/Entregable3/js/main.js
+++ Tp3/Entregable3/js/main.js
@@ -15,3 +15,19 @@ function playerRun(){
     player.className = "player";
 }
 
+let enemies = document.getElementById("enemies");
+document.querySelector("#playButton").addEventListener('click', enemyMovement);
+
+function enemyMovement(){
+    let value = 255;
+  let intervalo = setInterval(function(){
+    if(value < -45){
+        value = 255;
+    }
+    value -= 2;
+    let enemyMove = value.toString().concat("px");
+    enemies.style.left = enemyMove;
+  }, 255/15);
+
+
+}
